7d17697917e021ee64e4d6fc2e97c344bd162288
GalacticLemonade <90120199+GalacticLemonade@users.noreply.github.com>
2023-02-09 18:35:14 -0700

    Update KeySystem.lua

--- Functions/KeySystem.lua
+++ Functions/KeySystem.lua
@@ -1,1 +1,7 @@
+local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 
+local Window = Rayfield:CreateWindow({
+   Name = "",
+   LoadingTitle = "Rayfield Interface Suite",
+   LoadingSubtitle = "by Sirius",
+})
